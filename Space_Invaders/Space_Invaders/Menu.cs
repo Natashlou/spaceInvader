908e8234e5645c61b75950b13a2425f525a64968
Natasha Naylor <natasha.naylor@eduvaud.ch>
2022-11-09 16:26:27 +0100

    09.11.2022 16:26

--- Space_Invaders/Space_Invaders/Menu.cs
+++ Space_Invaders/Space_Invaders/Menu.cs
@@ -9,5 +9,26 @@ namespace Space_Invaders
     class Menu
     {
         
+       public Menu()
+        {
+            
+        } 
+
+        public void DrawMenu(int x, int y)
+        {
+            Console.WriteLine("Space Invaders\n");
+            Console.WriteLine("   Play");
+            Console.WriteLine("   help");
+            Console.WriteLine("   Scores");
+            Console.WriteLine("   Settings-Exit");
+            Console.SetCursorPosition(x, y);
+            Console.Write("->");
+        }
+
+        public void UpOrDown(ConsoleKey key)
+        {
+            
+                
+        }
     }
 }
--- Space_Invaders/Space_Invaders/Program.cs
+++ Space_Invaders/Space_Invaders/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Space_Invaders
 {
@@ -10,6 +11,30 @@ namespace Space_Invaders
     {
         static void Main(string[] args)
         {
+            int x = 0;
+            int y = 2;
+            
+            Menu m = new Menu();
+            m.DrawMenu(x, y);
+            ConsoleKey key = Console.ReadKey().Key;
+
+            while (key != ConsoleKey.Enter)
+            {
+                if (key == ConsoleKey.DownArrow && y != 5)
+                {
+                    
+                    Console.Clear();
+                    y = y + 1;
+                    m.DrawMenu(x, y);
+                }
+                else if (key == ConsoleKey.UpArrow && y != 2)
+                {
+                    Console.Clear();
+                    y = y - 1;
+                    m.DrawMenu(x, y);
+                }
+            } 
+
         }
     }
 }
